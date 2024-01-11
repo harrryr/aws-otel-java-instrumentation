3387adedb9c65dd76f6650e4e10b479be3889a75
ADOT Patch workflow <adot-patch-workflow@github.com>
2024-01-11 14:30:58 -0800

    Test

--- testing/terraform/ec2/main.tf
+++ testing/terraform/ec2/main.tf
@@ -154,7 +154,7 @@ resource "null_resource" "remote_service_setup" {
 
   provisioner "remote-exec" {
     inline = [
-      "yum update && yum install -y sudo",
+      "RUN yum update && yum install -y sudo",
       "usermod -aG sudo test",
       "sudo yum install wget",
       # Install Java 11 and tmux
