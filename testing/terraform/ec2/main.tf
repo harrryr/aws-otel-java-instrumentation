e877a7c4190bcab675cabc8361d7ec9cd2f8bd2c
ADOT Patch workflow <adot-patch-workflow@github.com>
2024-01-11 15:48:38 -0800

    Test wget and java11 install together

--- testing/terraform/ec2/main.tf
+++ testing/terraform/ec2/main.tf
@@ -150,9 +150,8 @@ resource "null_resource" "remote_service_setup" {
 
   provisioner "remote-exec" {
     inline = [
-      "yes | sudo yum install wget",
-      # Install Java 11 and tmux
-      "sudo yum install java-11-amazon-corretto -y",
+      # Install Java 11 and wget
+      "sudo yum install wget java-11-amazon-corretto -y",
 
       # Copy in CW Agent configuration
       "agent_config='${replace(replace(file("./amazon-cloudwatch-agent.json"), "/\\s+/", ""), "$REGION", var.aws_region)}'",
